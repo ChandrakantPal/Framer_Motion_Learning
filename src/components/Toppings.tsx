dbdcc266fde284df0ffb63d23d82db3c71b26ea2
ChandrakantPal <sanjay161097@gmail.com>
2021-05-27 19:59:52 +0530

    toppings.js to toppings.tsx

---
+++ src/components/Toppings.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+
+interface ToppingsProp {
+  addTopping: any
+  pizza: any
+}
+
+const Toppings: FC<ToppingsProp> = ({ addTopping, pizza }) => {
+  let toppings = [
+    'mushrooms',
+    'peppers',
+    'onions',
+    'olives',
+    'extra cheese',
+    'tomatoes',
+  ]
+
+  return (
+    <div className="toppings container">
+      <h3>Step 2: Choose Toppings</h3>
+      <ul>
+        {toppings.map((topping) => {
+          let spanClass = pizza.toppings.includes(topping) ? 'active' : ''
+          return (
+            <li key={topping} onClick={() => addTopping(topping)}>
+              <span className={spanClass}>{topping}</span>
+            </li>
+          )
+        })}
+      </ul>
+
+      <Link to="/order">
+        <button>Order</button>
+      </Link>
+    </div>
+  )
+}
+
+export default Toppings

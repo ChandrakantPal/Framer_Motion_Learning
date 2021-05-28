8f368df791455e21b7ca08c75c9c43e07f27dd29
ChandrakantPal <sanjay161097@gmail.com>
2021-05-28 22:43:15 +0530

    opacity animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,11 @@ import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
-    <motion.div className="home container" animate={{}}>
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
         <motion.button animate={{}}>Create Your Pizza</motion.button>

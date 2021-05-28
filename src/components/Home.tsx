a24f5a6e756e8944e85cf92c49e988496823b54a
ChandrakantPal <sanjay161097@gmail.com>
2021-05-28 22:36:18 +0530

    testing animation removed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,14 +4,10 @@ import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
-    <motion.div className="home container" animate={{ marginTop: 200 }}>
-      <motion.h2 animate={{ fontSize: 50, color: '#ff2994', x: 100, y: -100 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+    <motion.div className="home container" animate={{}}>
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{ scale: 1.5 }}>
-          Create Your Pizza
-        </motion.button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   )

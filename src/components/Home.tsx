d1353e29f4718a19973bb635af508fe5b69d6b6f
ChandrakantPal <sanjay161097@gmail.com>
2021-05-28 22:32:10 +0530

    motion imported animation tried

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div className="home container" animate={{ marginTop: 200 }}>
+      <motion.h2 animate={{ fontSize: 50, color: '#ff2994', x: 100, y: -100 }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
-export default Home;
+export default Home

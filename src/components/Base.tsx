cee04dae431dc7d180780a9643a7fbc0dcd3218a
ChandrakantPal <sanjay161097@gmail.com>
2021-06-28 16:04:52 +0530

    button variant added

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -35,6 +35,18 @@ const nextVariants = {
   },
 }
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+}
+
 const Base: FC<BaseProp> = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust']
 
@@ -65,13 +77,7 @@ const Base: FC<BaseProp> = ({ addBase, pizza }) => {
       {pizza.base && (
         <motion.div className="next" variants={nextVariants}>
           <Link to="/toppings">
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                textShadow: '0px 0px 8px rgb(255,255,255)',
-                boxShadow: '0px 0px 8px rgb(255,255,255)',
-              }}
-            >
+            <motion.button variants={buttonVariants} whileHover="hover">
               Next
             </motion.button>
           </Link>

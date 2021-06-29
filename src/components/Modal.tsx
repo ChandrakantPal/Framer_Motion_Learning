9975c2ece878c4cbefee7f4a16a383ab5a6700da
ChandrakantPal <sanjay161097@gmail.com>
2021-06-30 00:09:12 +0530

    modal with animation

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 
-const Modal = () => {
-  return <div></div>
+interface ModalProp {
+  showModal: boolean
+}
+
+const backdrop = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+}
+
+const Modal: FC<ModalProp> = ({ showModal }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdrop}
+          animate="visible"
+          initial="hidden"
+        ></motion.div>
+      )}
+    </AnimatePresence>
+  )
 }
 
 export default Modal

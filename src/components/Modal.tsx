78e536c876a74cef27aa680311fd8ade8cc91f17
ChandrakantPal <sanjay161097@gmail.com>
2021-06-29 23:53:34 +0530

    modal component created

---
+++ src/components/Modal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const Modal = () => {
+  return <div></div>
+}
+
+export default Modal

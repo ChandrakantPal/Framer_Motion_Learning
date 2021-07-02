2adec015c8015bd6a9e33eb505c2d916ea2066e4
ChandrakantPal <sanjay161097@gmail.com>
2021-07-02 16:31:10 +0530

    loader component created

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Loader = () => {
+  return (
+    <>
+      <motion.div className="loader"></motion.div>
+    </>
+  )
+}
+
+export default Loader

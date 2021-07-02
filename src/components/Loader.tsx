24fab1ee1e387798e8e4a99bd0562d6e84bf3177
ChandrakantPal <sanjay161097@gmail.com>
2021-07-02 16:40:00 +0530

    loader variants added

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+const loaderVariants = {
+  animationOne: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: {
+      x: {
+        yoyo: Infinity,
+        duration: 0.5,
+      },
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+      },
+    },
+  },
+}
+
 const Loader = () => {
   return (
     <>
-      <motion.div className="loader"></motion.div>
+      <motion.div
+        className="loader"
+        variants={loaderVariants}
+        animate="animationOne"
+      ></motion.div>
     </>
   )
 }

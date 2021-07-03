33056cd0442a36257c62a7fd1c7be0cf2b5c8ab2
ChandrakantPal <sanjay161097@gmail.com>
2021-07-03 21:49:57 +0530

    drag atributes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,12 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div
+        className="logo"
+        drag
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        dragElastic={0.7}
+      >
         <motion.svg
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +56,7 @@ const Header = () => {
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
           />
         </motion.svg>
-      </div>
+      </motion.div>
       <motion.div
         className="title"
         initial={{ y: -250 }}
